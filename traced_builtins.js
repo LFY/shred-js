be5ff44b1082db66600932fec2b55a221df2cdcb
Lingfeng Yang <lyang@cs.stanford.edu>
2014-03-29 11:38:50 -0700

    Make traced_builtins use the new library.

--- traced_builtins.js
+++ traced_builtins.js
@@ -3,67 +3,29 @@
 _ = require("underscore");
 shred = require("./shred");
 church = require("./webchurch/church_builtins");
+bt = require("./builtin_transformations.js");
 
 // Begin module generation======================================================
 
-module.exports.__annotations__ = {};
+var untraced_primitives = [
+        "iota", "make_list", "repeat", "map", "for_each", "reverse", // more constructive
+        "filter", "take", "length", "sum", // more destructive
+        "append", "flatten", // construct/destruct lists of lists
+        "fold" // everything
+            ];
 
-var untraced_primitives = ["map"];
+module.exports.untraced_primitives = untraced_primitives;
 
-var list_primitives = function (module) {
-    var res = [];
-    var non_primitives = untraced_primitives.concat(["__annotations__", "_const", "list_primitives", "args_to_list", "wrapped_mh_query", "wrapped_rejection_query", "wrapped_enumeration_query", "wrapped_eval", "read_file", "read_csv", "bootstrap", "update_list"]);
-    for (name in module) {
-        if (!_.contains(non_primitives, name)) {
-            res.push([name, module[name]]);
-        }
-    }
-    return res;
-}
+synth_builtins = bt.synthesize_builtins(church, shred.shred, untraced_primitives);
 
-var addBuiltin = function(dict) {
-  var fWrapped = dict.fn;
-  
-  module.exports[dict.name] = fWrapped;
-  module.exports.__annotations__[dict.name] = dict;
-  return fWrapped;
-};
-
-// From webchurch/js_astify.js
-var deep_copy = function(x) { return JSON.parse(JSON.stringify(x)); }
-
-var copyAnnotationDict = function(dict) {
-    res = {}
-    res.name = dict.name
-    res.alias = dict.alias
-    res.params = deep_copy(dict.params)
-    res.fn = "NYI"
-    return res
-}
-
-var churchprims = list_primitives(church);
-
-for (idx in churchprims) {
-
-    name = churchprims[idx][0];
-    func = churchprims[idx][1];
-    // console.log(name);
-    orig_dict = church.__annotations__[name];
-
-    shred_func = shred.shred(name, func);
-
-    shred_dict = copyAnnotationDict(orig_dict);
-
-    shred_dict.fn = shred_func;
-
-    // console.log(shred_dict);
-    addBuiltin(shred_dict);
-}
+module.exports = synth_builtins.exports;
+module.exports.__annotations__ = synth_builtins.exports.__annotations__;
 
 // Important stuff
 
 module.exports._const = shred._const;
 module.exports._if = shred._if;
+module.exports.dump_trace = shred.dump_trace
 
 // Patch things up
 
@@ -98,5 +60,4 @@ for (idx in untraced_primitives) {
     }
 }
 
-module.exports.dump_trace = shred.dump_trace
 
