9776e577b0b362fafdbc26d42968701d80d2aaf7
Lingfeng Yang <lyang@cs.stanford.edu>
2014-03-29 15:29:05 -0700

    Backward slicing as a "delayed retraction" of the trace.

--- sliced_builtins.js
+++ sliced_builtins.js
@@ -32,9 +32,46 @@ function advance_slice_state(lhsv, procname, rhsvs) {
         SLICE_STATE.slice.push([lhsv, procname, rhsvs]);
     }
 }
+
+function retract_slice_state(lhsv, procname, rhsvs) {
+    function remove_elt(items, item) {
+        while (items.indexOf(item) !== -1) {
+            items.splice(items.indexOf(item), 1);
+        }
+        return items
+    }
+
+    var call = function(deplist, k) {
+        if (deplist == []) { return k([]); } else {
+            console.log(deplist)
+            if (_.contains(deplist, lhsv)) {
+                SLICE_STATE.slice.push([lhsv, procname, rhsvs]);
+                // var next_deplist = _.filter(deplist, function (x) { x != lhsv });
+                for (var i = 0; i < rhsvs.length; i++) {
+            console.log("PUSH");
+                    if (!(_.contains(deplist, rhsvs[i]))) {
+                        deplist.push(rhsvs[i]);
+                    }
+                }
+                return k(deplist);
+            } else {
+                return k(deplist);
+            }
+        }
+    }
+    var curr_idx = SLICE_STATE.depk.length - 1;
+    var next_step = function (deplist) {
+        return call(deplist, SLICE_STATE.depk[curr_idx]);
+    };
+    SLICE_STATE.depk.push(next_step);
+}
 function set_slice_from(retv, procname, callvs) { 
     SLICE_STATE.slice_variables = [retv]; 
     SLICE_STATE.slice = [[retv, procname, callvs]];
+}
+
+function bwd_slice_of(v, procname, callvs) {
+    SLICE_STATE.depk[SLICE_STATE.depk.length - 1]([v]);
 }
 function dump_slice() { 
     return shred.dump_stmt_list(SLICE_STATE.slice); 
@@ -52,6 +89,7 @@ function fwdsliced(name, trace_proc) {
         var stmt = [retvar, name, call_vars];
 
         advance_slice_state(retvar, name, call_vars);
+        retract_slice_state(retvar, name, call_vars);
 
         return retvar;
     }
@@ -64,4 +102,5 @@ module.exports.__annotations__ = synth_builtins.exports.__annotations__;
 module.exports._const = retraced._const;
 module.exports.dump_trace = shred.dump_trace;
 module.exports.set_slice_from = set_slice_from;
+module.exports.bwd_slice_of = bwd_slice_of;
 module.exports.dump_slice  = dump_slice;
