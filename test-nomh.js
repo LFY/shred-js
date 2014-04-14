35a681dff7f71f1373a3581fa0d9f1b7e3b43e47
Lingfeng Yang <lyang@cs.stanford.edu>
2014-04-14 14:29:30 -0700

    Test address-based trace variables.

--- test-nomh.js
+++ test-nomh.js
@@ -2,16 +2,20 @@
 __pr = require("./webchurch/probabilistic-js/probabilistic/index"); __pr.openModule(__pr);
 __ch = require("./traced_builtins"); __pr.openModule(__ch);
 __int = require("./trace_interface");
-enterfn(0);
-var call0 = _const(3.141592653589793);
-leavefn();
-var my_45pi = call0;
-var glp = function (mean, ssq, smp) {
-    enterfn(14);
-    var call14 = minus(smp, mean);
+__sh = require("./shred");
+
+function nfqp() {
+    __sh.reset_trace_state();
+    enterfn(0);
+    var call0 = _const(3.141592653589793);
     leavefn();
-    enterfn(15);
-    var call15 = function (diff) {
+    var my_45pi = call0;
+    var glp = function (mean, ssq, smp) {
+        enterfn(14);
+        var call14 = minus(smp, mean);
+        leavefn();
+        enterfn(15);
+        var call15 = function (diff) {
             enterfn(1);
             var call1 = mult(diff, diff);
             leavefn();
@@ -53,34 +57,34 @@ var glp = function (mean, ssq, smp) {
             leavefn();
             return call13;
         }(call14);
-    leavefn();
-    return call15;
-};
-var randint = function (l, h) {
-    enterfn(16);
-    var call16 = _const(1);
-    leavefn();
-    enterfn(17);
-    var call17 = minus(h, l);
-    leavefn();
-    enterfn(18);
-    var call18 = plus(call16, call17);
-    leavefn();
-    enterfn(19);
-    var call19 = wrapped_random_integer(call18);
-    leavefn();
-    enterfn(20);
-    var call20 = plus(l, call19);
-    leavefn();
-    return call20;
-};
-var index_45in = function (x, xs) {
-    var loop = function (x, k, rst) {
-        enterfn(21);
-        var call21 = is_null(rst);
         leavefn();
-        enterfn(29);
-        var call29 = _if(call21, function () {
+        return call15;
+    };
+    var randint = function (l, h) {
+        enterfn(16);
+        var call16 = _const(1);
+        leavefn();
+        enterfn(17);
+        var call17 = minus(h, l);
+        leavefn();
+        enterfn(18);
+        var call18 = plus(call16, call17);
+        leavefn();
+        enterfn(19);
+        var call19 = wrapped_random_integer(call18);
+        leavefn();
+        enterfn(20);
+        var call20 = plus(l, call19);
+        leavefn();
+        return call20;
+    };
+    var index_45in = function (x, xs) {
+        var loop = function (x, k, rst) {
+            enterfn(21);
+            var call21 = is_null(rst);
+            leavefn();
+            enterfn(29);
+            var call29 = _if(call21, function () {
                 return k;
             }, function () {
                 enterfn(22);
@@ -91,42 +95,42 @@ var index_45in = function (x, xs) {
                 leavefn();
                 enterfn(28);
                 var call28 = _if(call23, function () {
-                        return k;
-                    }, function () {
-                        enterfn(24);
-                        var call24 = _const(1);
-                        leavefn();
-                        enterfn(25);
-                        var call25 = plus(k, call24);
-                        leavefn();
-                        enterfn(26);
-                        var call26 = rest(rst);
-                        leavefn();
-                        enterfn(27);
-                        var call27 = loop(x, call25, call26);
-                        leavefn();
-                        return call27;
-                    });
+                    return k;
+                }, function () {
+                    enterfn(24);
+                    var call24 = _const(1);
+                    leavefn();
+                    enterfn(25);
+                    var call25 = plus(k, call24);
+                    leavefn();
+                    enterfn(26);
+                    var call26 = rest(rst);
+                    leavefn();
+                    enterfn(27);
+                    var call27 = loop(x, call25, call26);
+                    leavefn();
+                    return call27;
+                });
                 leavefn();
                 return call28;
             });
+            leavefn();
+            return call29;
+        };
+        enterfn(30);
+        var call30 = _const(0);
         leavefn();
-        return call29;
+        enterfn(31);
+        var call31 = loop(x, call30, xs);
+        leavefn();
+        return call31;
     };
-    enterfn(30);
-    var call30 = _const(0);
-    leavefn();
-    enterfn(31);
-    var call31 = loop(x, call30, xs);
-    leavefn();
-    return call31;
-};
-var my_45length = function (xs) {
-    enterfn(32);
-    var call32 = is_null(xs);
-    leavefn();
-    enterfn(38);
-    var call38 = _if(call32, function () {
+    var my_45length = function (xs) {
+        enterfn(32);
+        var call32 = is_null(xs);
+        leavefn();
+        enterfn(38);
+        var call38 = _if(call32, function () {
             enterfn(33);
             var call33 = _const(0);
             leavefn();
@@ -146,18 +150,18 @@ var my_45length = function (xs) {
             leavefn();
             return call37;
         });
-    leavefn();
-    return call38;
-};
-var data_45loop = function (n) {
-    enterfn(39);
-    var call39 = _const(51);
-    leavefn();
-    enterfn(40);
-    var call40 = eq(n, call39);
-    leavefn();
-    enterfn(47);
-    var call47 = _if(call40, function () {
+        leavefn();
+        return call38;
+    };
+    var data_45loop = function (n) {
+        enterfn(39);
+        var call39 = _const(51);
+        leavefn();
+        enterfn(40);
+        var call40 = eq(n, call39);
+        leavefn();
+        enterfn(47);
+        var call47 = _if(call40, function () {
             enterfn(41);
             var call41 = _const([null]);
             leavefn();
@@ -180,74 +184,74 @@ var data_45loop = function (n) {
             leavefn();
             return call46;
         });
-    leavefn();
-    return call47;
-};
-enterfn(48);
-var call48 = _const(0);
-leavefn();
-enterfn(49);
-var call49 = data_45loop(call48);
-leavefn();
-var data = call49;
-enterfn(50);
-var call50 = map(first, data);
-leavefn();
-enterfn(51);
-var call51 = sum(call50);
-leavefn();
-enterfn(52);
-var call52 = my_45length(data);
-leavefn();
-enterfn(53);
-var call53 = div(call51, call52);
-leavefn();
-var xbar = call53;
-var make_45sample = function () {
-    enterfn(54);
-    var call54 = _const(0);
-    leavefn();
-    enterfn(55);
-    var call55 = _const(10000);
-    leavefn();
-    enterfn(56);
-    var call56 = wrapped_gaussian(call54, call55);
-    leavefn();
-    var alpha = call56;
-    enterfn(57);
-    var call57 = _const(0);
-    leavefn();
-    enterfn(58);
-    var call58 = _const(10000);
-    leavefn();
-    enterfn(59);
-    var call59 = wrapped_gaussian(call57, call58);
-    leavefn();
-    var wrapped_beta = call59;
-    enterfn(60);
-    var call60 = _const(0);
-    leavefn();
-    enterfn(61);
-    var call61 = _const(10000);
-    leavefn();
-    enterfn(62);
-    var call62 = wrapped_gaussian(call60, call61);
-    leavefn();
-    enterfn(63);
-    var call63 = abs(call62);
-    leavefn();
-    var tau = call63;
-    var gauss_45factor = function (m, v, x) {
-        enterfn(64);
-        var call64 = glp(m, v, x);
         leavefn();
-        enterfn(65);
-        var call65 = wrapped_factor(call64);
-        leavefn();
-        return call65;
+        return call47;
     };
-    enterfn(74);
-    var call74 = map(function (xy) {
+    enterfn(48);
+    var call48 = _const(0);
+    leavefn();
+    enterfn(49);
+    var call49 = data_45loop(call48);
+    leavefn();
+    var data = call49;
+    enterfn(50);
+    var call50 = map(first, data);
+    leavefn();
+    enterfn(51);
+    var call51 = sum(call50);
+    leavefn();
+    enterfn(52);
+    var call52 = my_45length(data);
+    leavefn();
+    enterfn(53);
+    var call53 = div(call51, call52);
+    leavefn();
+    var xbar = call53;
+    var make_45sample = function () {
+        enterfn(54);
+        var call54 = _const(0);
+        leavefn();
+        enterfn(55);
+        var call55 = _const(10000);
+        leavefn();
+        enterfn(56);
+        var call56 = wrapped_gaussian(call54, call55);
+        leavefn();
+        var alpha = call56;
+        enterfn(57);
+        var call57 = _const(0);
+        leavefn();
+        enterfn(58);
+        var call58 = _const(10000);
+        leavefn();
+        enterfn(59);
+        var call59 = wrapped_gaussian(call57, call58);
+        leavefn();
+        var wrapped_beta = call59;
+        enterfn(60);
+        var call60 = _const(0);
+        leavefn();
+        enterfn(61);
+        var call61 = _const(10000);
+        leavefn();
+        enterfn(62);
+        var call62 = wrapped_gaussian(call60, call61);
+        leavefn();
+        enterfn(63);
+        var call63 = abs(call62);
+        leavefn();
+        var tau = call63;
+        var gauss_45factor = function (m, v, x) {
+            enterfn(64);
+            var call64 = glp(m, v, x);
+            leavefn();
+            enterfn(65);
+            var call65 = wrapped_factor(call64);
+            leavefn();
+            return call65;
+        };
+        enterfn(74);
+        var call74 = map(function (xy) {
             enterfn(66);
             var call66 = first(xy);
             leavefn();
@@ -274,29 +278,36 @@ var make_45sample = function () {
             leavefn();
             return call73;
         }, data);
+        leavefn();
+        var y_45constrs = call74;
+        enterfn(75);
+        var call75 = _const(1);
+        leavefn();
+        enterfn(76);
+        var call76 = _const(0.5);
+        leavefn();
+        enterfn(77);
+        var call77 = expt(tau, call76);
+        leavefn();
+        enterfn(78);
+        var call78 = div(call75, call77);
+        leavefn();
+        enterfn(79);
+        var call79 = list(alpha, wrapped_beta, call78, tau);
+        leavefn();
+        var sample = call79;
+        return sample;
+    };
+    enterfn(80);
+    var call80 = make_45sample();
     leavefn();
-    var y_45constrs = call74;
-    enterfn(75);
-    var call75 = _const(1);
-    leavefn();
-    enterfn(76);
-    var call76 = _const(0.5);
-    leavefn();
-    enterfn(77);
-    var call77 = expt(tau, call76);
-    leavefn();
-    enterfn(78);
-    var call78 = div(call75, call77);
-    leavefn();
-    enterfn(79);
-    var call79 = list(alpha, wrapped_beta, call78, tau);
-    leavefn();
-    var sample = call79;
+    var sample = call80;
     return sample;
-};
-enterfn(80);
-var call80 = make_45sample();
-leavefn();
-var sample = call80;
-undefined
+}
+
+var tr = newTrace(nfqp, "nothing");
+__sh.trace_cell.trace = tr;
+
+tr.reset();
+
 console.log(dump_trace());
